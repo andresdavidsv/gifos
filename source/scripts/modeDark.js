ff0a3fe8c7e29d5a081bd4fde498b68d41807cdf
krincon36 <krincon36@uan.edu.co>
2021-06-16 08:42:09 -0500

    create/fix/layout/styles

--- source/scripts/modeDark.js
+++ source/scripts/modeDark.js
@@ -1,4 +1,7 @@
 const BModoNocturno = document.querySelector('#switch')
+const logo=document.querySelector('#logoMobile')
+const camara=document.querySelector('#camara')
+const cinta=document.querySelector('#cinta')
 console.log(BModoNocturno)
 
 BModoNocturno.addEventListener('click',()=>{
@@ -7,11 +10,17 @@ BModoNocturno.addEventListener('click',()=>{
         localStorage.setItem("modeDark", "true");
     
         BModoNocturno.textContent = "Modo Diurno";
+        logo.src="./assets/logo-dark-mode.svg";
+        camara.src = "./assets/illustration-camera-dark.svg";
+        cinta.src = "./assets/Illustration-movie-dark.svg";
     
       } else {
         localStorage.setItem("modeDark", "false");
     
         BModoNocturno.textContent = "Modo Nocturno";
+        logo.src = "./assets/logo.svg";
+        camara.src = "./assets/illustration-camera.svg";
+        cinta.src = "./assets/illustration-movie.svg";
     
       }
     });
@@ -20,6 +29,9 @@ if (localStorage.getItem("modeDark") === "true") {
   document.body.classList.add("modoNocturno");
 
   BModoNocturno.innerHTML = "Modo Diurno";
+  logo.src = "./assets/logo-dark-mode.svg";
+  camara.src = "./assets/illustration-camera-dark.svg";
+  cinta.src = "./assets/Illustration-movie-dark.svg";
 
 } else {
   document.body.classList.remove("modoNocturno");
